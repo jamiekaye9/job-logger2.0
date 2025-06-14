95e8368d9bc086a906837ed6964ed853eb3ea14a
Jamie Kaye <jamie.kaye4@googlemail.com>
2025-06-14 15:34:52 +0100

    home page styling updated

--- frontend/client/src/components/Home.jsx
+++ frontend/client/src/components/Home.jsx
@@ -1,7 +1,10 @@
-import landingPicture from '../assets/landing-picture.png';
 import apply from '../assets/apply.jpg'
 import register from '../assets/sign-up.jpg';
 import interview from '../assets/interview.jpg';
+import linkedin from '../assets/linkedin.png';
+import google from '../assets/google.png';
+import glassdoor from '../assets/glassdoor.png';
+import indeed from '../assets/indeed.png';
 import { Link } from 'react-router-dom';
 
 
@@ -9,7 +12,7 @@ const Home = () => {
   return (
     <div className='container'>
        <div className="row d-flex justify-content-center align-items-center">
-        <div className="col-11 col-lg-8 card bg-dark text-light mt-4 rounded-4 d-flex flex-column justify-content-center align-items-center">
+        <div className="col-11 col-lg-9 card bg-dark text-light mt-5 mb-4 rounded-4 d-flex flex-column justify-content-center align-items-center">
             <div className="card-body">
                 <h1 className='card-title source-serif text-center mb-4 mt-4'>Welcome to <span className='text-light'>JOB</span><span className='text-info'>LOGGER.</span></h1>
                 <h3 className='card-subtitle display-5 fs-5 text-center'>Your Ultimate Job Application Companion</h3>
@@ -20,24 +23,40 @@ const Home = () => {
             </div>
         </div>
        </div>
-       <div className="row d-flex justify-content-center align-items-center mt-5">
-        <div>
-            <div className="col-8 card">
-                <div className="card-header">
-                    <h2 className='card-title'>Apply</h2>
+       <div className="row d-flex flex-column flex-lg-row justify-content-center align-items-center mt-5 mb-5">
+            <div className="col-7 col-lg-3 card rounded-4 border-0 mb-4">
+                <div className="card-header bg-dark text-info rounded-top-4">
+                    <h2 className='card-title source-serif text-center fs-4'>Register</h2>
                 </div>
-                <div className="card-body bg-dark opacity-50">
-                    <img src={register} alt="" />
+                <img src={register} alt="" className='rounded-bottom-4' />
+            </div>
+            <div className="col-7 col-lg-3 card rounded-4 border-0 mb-4">
+                <div className="card-header bg-dark text-info rounded-top-4">
+                    <h2 className='card-title source-serif text-center fs-4'>Apply</h2>
                 </div>
+                <img src={apply} alt="" className='rounded-bottom-4' />
             </div>
-            <div className="col-8 card">
-                <img src={apply} alt="" />
-            </div>
-            <div className="col-8 card">
-                <img src={interview} alt="" />
+            <div className="col-7 col-lg-3 card rounded-4 border-0 mb-4">
+                <div className="card-header bg-dark text-info rounded-top-4">
+                    <h2 className='card-title source-serif text-center fs-4'>Progress & Track</h2>
+                </div>
+                <img src={interview} alt="" className='rounded-bottom-4' />
             </div>
         </div>
-       </div>
+        <div className='row d-flex justify-content-center align-items-center'>
+            <div className='col-11 col-lg-9 card rounded-4 d-flex flex-column justify-content-center align-items-center p-0 mb-5 border-1 border-dark'>
+                <div className='card-header bg-info text-dark rounded-top-4 w-100'>
+                    <h1 className='card-title text-center source-serif mt-4 mb-3'>Start Applying</h1>
+                    <p className='card-text display-5 fs-5 text-center mb-3'>Visit the Industry Leading job sites below to find your next role</p>
+                </div>
+                <div className='d-flex flex-row gap-3 gap-md-4 gap-lg-5 justify-content-center align-items-center mt-4 mb-4'>
+                    <a href="/https://linkedin.com"><img src={linkedin} alt="" style={{ width: "80px"}} /></a>
+                    <a href="/https://google.com"><img src={google} alt="" style={{ width: "80px"}} /></a>
+                    <a href="/https://glassdoor.com"><img src={glassdoor} alt="" style={{ width: "65px"}} /></a>
+                    <a href="/https://indeed.com"><img src={indeed} alt="" style={{ width: "80px"}} /></a>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
